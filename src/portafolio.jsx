771e15171df4b26d74849254619589ccad64e124
NisalWijethunga <89016922+NisalWijethunga@users.noreply.github.com>
2024-04-17 10:53:58 +0530

    add

---
+++ src/portafolio.jsx
@@ -0,0 +1,12 @@
+const about = {
+    name: 'Nisal Wijethunga',
+    role: 'Software Developer',
+    description:
+      'As a versatile software developer, I specialize in crafting robust solutions across multiple platforms, leveraging my expertise in Java, Flutter, and React.js. With a strong foundation in Java, I excel in building scalable backend systems, enterprise applications, and Android mobile apps. My proficiency extends to utilizing Java frameworks like Spring and Hibernate to streamline development processes and enhance project efficiency.',
+    resume: '',
+    social: {
+      linkedin: 'https://www.linkedin.com/in/nisal-wijethunge/',
+      github: 'https://github.com/NisalWijethunga',
+    },
+  }
+  export {about}
