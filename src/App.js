c203e950e01f2ef3e2ac8e906ed4ed06f8bd682e
NisalWijethunga <89016922+NisalWijethunga@users.noreply.github.com>
2024-04-17 10:33:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>
-          Portafolio
-        </h1>
-      </header>
+     <h1>Hello</h1>
     </div>
   );
 }
