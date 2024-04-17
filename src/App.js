53ff1e959ef7f258c3d5eb551bcc0dd5c57df7fd
NisalWijethunga <89016922+NisalWijethunga@users.noreply.github.com>
2024-04-17 15:18:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
         
           <Routes>
           <Route path='/' element={<Home/> } />
-          <Route path='/about' element={<About/>} />
+          <Route path='/About' element={<About/>} />
 
 
           </Routes>
