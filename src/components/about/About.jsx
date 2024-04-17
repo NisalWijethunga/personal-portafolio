8e73ea91d41d4883dc2564f53a2086dc4737ef5c
NisalWijethunga <89016922+NisalWijethunga@users.noreply.github.com>
2024-04-17 12:10:00 +0530

    add

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,9 @@
-import GitHubIcon from '@material-ui/icons/GitHub'
-import LinkedInIcon from '@material-ui/icons/LinkedIn'
-import { about } from '../../portfolio'
+// import GitHubIcon from '@material-ui/icons/GitHub'
+// import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import {about} from "../../portafolio";
 
 const About = () => {
-    const { name, role, description, resume, social } = about
+    const { name, role, description, resume, social } = About
     
   
     return (
@@ -34,7 +34,7 @@ const About = () => {
                   aria-label='github'
                   className='link link--icon'
                 >
-                  <GitHubIcon />
+                  {/* <GitHubIcon /> */}
                 </a>
               )}
   
@@ -44,7 +44,7 @@ const About = () => {
                   aria-label='linkedin'
                   className='link link--icon'
                 >
-                  <LinkedInIcon />
+                  {/* <LinkedInIcon /> */}
                 </a>
               )}
             </>
