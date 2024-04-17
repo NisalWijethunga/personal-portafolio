e55553567bedd3b9910f4994d4f0c24fe14668b4
NisalWijethunga <89016922+NisalWijethunga@users.noreply.github.com>
2024-04-17 11:22:38 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import About from './components/about/about';
 import './App.css';
 
 const App = () => {
