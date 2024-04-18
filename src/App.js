af2edc593f66639e9f067b6d6a24253a2668e829
NisalWijethunga <89016922+NisalWijethunga@users.noreply.github.com>
2024-04-18 08:51:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
+import { useContext } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeContext } from './contexts/theme';
 import Home from './components/Home';
 import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
+import ScrollToTop from './components/ScrollToTop';
 import './App.css';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
-
 const App = () => {
+  const [{ themeName }] = useContext(ThemeContext);
   return (
-    
+    <div id='top' className={`${themeName} app`}>
       <BrowserRouter>
 
         
@@ -24,6 +27,7 @@ const App = () => {
           <Route path='/Skills' element={<Skills/>} />
           <Route path='/Header' element={<Header/>} />
           <Route path='/Contact' element={<Contact/>} />
+          <Route path='/ScrollToTop' element={<ScrollToTop/>} />
           <Route path='/Footer' element={<Footer/>} />
 
 
@@ -31,7 +35,7 @@ const App = () => {
         
       </BrowserRouter>
    
-     
+     </div>
 
   );
 };
